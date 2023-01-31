97c858f7f5ad6b47f69bcc00dc19d7e2f7539d9c
Giovanni Montanari <giovannimontanari3@gmail.com>
2023-01-30 21:31:50 -0300

    create aligment items

--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -45,7 +46,9 @@ class MainActivity : ComponentActivity() {
                     .padding(padding)
                     .background(Color.White)
                     .fillMaxWidth()
-                    .fillMaxHeight()
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text("Text 1")
                 Text("Text 2")
